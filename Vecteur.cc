ed1bea55b248518d7d4492629d1fc5b5a910ebd5
bogdy111 <bogdan.secas@epfl.ch>
2014-03-18 14:58:11 +0100

    Update Vecteur.cc

--- Vecteur.cc
+++ Vecteur.cc
@@ -3,7 +3,24 @@
 #include "Vecteur.h"
 #include <cmath>
 
-using namespace std;  // les commentaires sont cool
+using namespace std;
+
+Vecteur::Vecteur(double X, double Y, double Z){
+			x = X;
+			y = Y;
+			z = Z;
+		}
+		
+Vecteur::Vecteur(const Vecteur& a){
+			x = a.x;
+			y = a.y;
+			z = a.z;
+		}
+Vecteur::Vecteur(){
+			x = 0.0;
+			y = 0.0;
+			z = 0.0;
+		}
 
 double Vecteur::getX(){
 	return x;
@@ -23,64 +40,57 @@ void Vecteur::setY(double a){
 void Vecteur::setZ(double a){
 	z = a;
 }
-bool Vecteur::compare(Vecteur a){ // Est vrai si les vecteurs sont identiques
-	if (a.getX() == x){
-		if (a.getY() == y){
-			if (a.getZ() == z){
-				return true;
-			}else{
-				return false;
-            }
-        }else{
-            return false;
-        }
-    }else{
-    return false;
-	}
+
+void Vecteur::affiche(ostream& sortie){   // affiche les coordonnées du vecteur, séparées par un espace
+			cout << x << " " << y << " " << z;
 }
 
-
-void Vecteur::affiche(){
-	cout << x << " " << y << " " << z;
+double Vecteur::norme() const{    //retourne la norme du vecteur
+	return sqrt( x*x + y*y + z*z);
 }
 
-Vecteur Vecteur::addition(Vecteur autre) const{
+double Vecteur::norme_carree() const {    // norme au carré
+	double a;
+	a = Vecteur::norme();
+	return a*a;
+}
+
+bool Vecteur::operator==(Vecteur a){  // est égal à ...
+	return ((a.getX() == x) and (a.getY() == y) and (a.getZ() == z));
+}
+
+bool Vecteur::operator!=(Vecteur a){
+	return not ((a.getX() == x) and (a.getY() == y) and (a.getZ() == z)); // est différent de...
+}
+
+ostream& operator<<(ostream& sortie, Vecteur& a){  // s'utillise avec la commande:   cout << nom_vecteur;
+	a.affiche(sortie);
+	return sortie;
+}
+
+Vecteur Vecteur::operator+(Vecteur autre){ // addition
 	Vecteur c(autre.getX() + x, autre.getY() + y, autre.getZ() + z);
-return c;
+	return c;	
 }
 
-Vecteur Vecteur::soustraction(Vecteur autre) const{
-	Vecteur c(x - autre.getX(),y - autre.getY(),z - autre.getZ());
-return c;
+Vecteur Vecteur::operator-(Vecteur autre){  // soustraction 
+	Vecteur c(autre.getX() - x, autre.getY() - y, autre.getZ() - z);
+	return c;	
 }
 
-Vecteur Vecteur::oppose() const {
-	Vecteur c(-x, -y , -z);
-	return c;
-}
-Vecteur Vecteur::mult(double a) const{
-	Vecteur c( a*x, a*y, a*z);
-	return c;
-}
-double Vecteur::prod_scal(Vecteur autre) const{
+double Vecteur::operator*(Vecteur autre){   //produit scalaire
 	double a;
 	a = autre.getX()*x + autre.getY()*y + autre.getZ()*z;
 	return a;
 }
-Vecteur Vecteur::prod_vect(Vecteur a) const{
+
+Vecteur Vecteur::operator*(double a){  //surcharge d'opérateurs: multiplication d'un vecteur par un scalaire
+	Vecteur c(a*x, a*y, a*z);
+	return c;
+}
+
+Vecteur Vecteur::operator^(Vecteur a){
 	Vecteur c(y*a.getZ()- z*a.getY(), z*a.getX() - x*a.getZ(), x*a.getY() - y*a.getX());
 	return c;
 }
 
-double Vecteur::norme() const{
-	double a;
-	a = sqrt( x*x + y*y + z*z);
-	return a;
-}
-
-double Vecteur::norme_carree() const {
-	double a;
-	a = Vecteur::norme();
-	a = a*a;
-	return a;
-}

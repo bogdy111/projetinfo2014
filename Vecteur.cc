569b2c62b60d5d365e8cff41fc5962b5e89392d9
vardrien <adrien.varlez@gmail.com>
2014-03-18 14:13:57 +0100

    Create Vecteur.cc

---
+++ Vecteur.cc
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+#include "Vecteur.h"
+#include <cmath>
+
+using namespace std;
+
+double Vecteur::getX(){
+	return x;
+}
+double Vecteur::getY(){
+	return y;
+}
+double Vecteur::getZ(){
+	return z;
+}
+void Vecteur::setX(double a){
+	x = a;
+}
+void Vecteur::setY(double a){
+	y = a;
+}
+void Vecteur::setZ(double a){
+	z = a;
+}
+bool Vecteur::compare(Vecteur a){ // Est vrai si les vecteurs sont identiques
+	if (a.getX() == x){
+		if (a.getY() == y){
+			if (a.getZ() == z){
+				return true;
+			}else{
+				return false;
+            }
+        }else{
+            return false;
+        }
+    }else{
+    return false;
+	}
+}
+
+
+void Vecteur::affiche(){
+	cout << x << " " << y << " " << z;
+}
+
+Vecteur Vecteur::addition(Vecteur autre) const{
+	Vecteur c(autre.getX() + x, autre.getY() + y, autre.getZ() + z);
+return c;
+}
+
+Vecteur Vecteur::soustraction(Vecteur autre) const{
+	Vecteur c(x - autre.getX(),y - autre.getY(),z - autre.getZ());
+return c;
+}
+
+Vecteur Vecteur::oppose() const {
+	Vecteur c(-x, -y , -z);
+	return c;
+}
+Vecteur Vecteur::mult(double a) const{
+	Vecteur c( a*x, a*y, a*z);
+	return c;
+}
+double Vecteur::prod_scal(Vecteur autre) const{
+	double a;
+	a = autre.getX()*x + autre.getY()*y + autre.getZ()*z;
+	return a;
+}
+Vecteur Vecteur::prod_vect(Vecteur a) const{
+	Vecteur c(y*a.getZ()- z*a.getY(), z*a.getX() - x*a.getZ(), x*a.getY() - y*a.getX());
+	return c;
+}
+
+double Vecteur::norme() const{
+	double a;
+	a = sqrt( x*x + y*y + z*z);
+	return a;
+}
+
+double Vecteur::norme_carree() const {
+	double a;
+	a = Vecteur::norme();
+	a = a*a;
+	return a;
+}
